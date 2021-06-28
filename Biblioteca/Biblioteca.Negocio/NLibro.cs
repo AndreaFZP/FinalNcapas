bd987a527f3f2f42dea8753f6c6d7ce2d4079e8d
Andrea <54257897+AndreaFZP@users.noreply.github.com>
2021-06-28 01:16:27 -0600

    Actualizar y eliminar libros agregado

--- Biblioteca/Biblioteca.Negocio/NLibro.cs
+++ Biblioteca/Biblioteca.Negocio/NLibro.cs
@@ -30,5 +30,32 @@ namespace Biblioteca.Negocio
             return Datos.Insertar(Obj);
 
         }
+
+        //Actualizar libro
+        public static string Actualizar(int id, int nejemplares, string ISBN, string titulo, string autor, string editorial, int anio, int nedicion, string pais, string idioma, string materia)
+        {
+            DLibro Datos = new DLibro();
+
+            Libro Obj = new Libro();
+            Obj.CodigoLibro = id;
+            Obj.nEjemplares = nejemplares;
+            Obj.isbn = ISBN;
+            Obj.Titulo = titulo;
+            Obj.Autor = autor;
+            Obj.Editorial = editorial;
+            Obj.Anio = anio;
+            Obj.nEdicion = nedicion;
+            Obj.Pais = pais;
+            Obj.Idioma = idioma;
+            Obj.Materia = materia;
+            return Datos.Actualizar(Obj);
+        }
+
+        //Eliminar Libro
+        public static string Eliminar(int id)
+        {
+            DLibro Datos = new DLibro();
+            return Datos.Eliminar(id);
+        }
     }
 }
